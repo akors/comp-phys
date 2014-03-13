ed846446800e5d6d7dec0e41dd6c55299ff12924
Alexander Korsunsky <fat.lobyte9@gmail.com>
2014-03-13 14:51:06 +0100

    Implement simple euler solver

--- blatt1/pendulum.cpp
+++ blatt1/pendulum.cpp
@@ -1,14 +1,26 @@
 #include <string>
 #include <map>
+#include <valarray>
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
 
-
 #define _USE_MATH_DEFINES
 #include <cmath>
+
+// initialize with default paramters
+static std::map<std::string, double> params {
+        {"stepsize", 0.001},
+        {"t_init", 0.0}, // initial conditions
+        {"y_init", 1.0},
+        {"t_end", 3},
+        {"theta0", 0},
+        {"dotheta0", 2*M_PI},
+        {"k_exp", 0.33}
+};
+
 
 void readParams(std::map<std::string, double>& parms, std::ifstream& parmfile)
 {
@@ -42,15 +54,36 @@ void readParams(std::map<std::string, double>& parms, std::ifstream& parmfile)
     }
 }
 
+template <typename T, typename F> 
+struct solver_euler
+{
+    T t_cur, y_cur; // current t/y values
+    F f; // functor depending on t,y
+
+    // initializing constructor
+    solver_euler(const F& func, T t_init, T y_init)
+        : f(func), t_cur(t_init), y_cur(y_init)
+    {     
+    }
+
+    // no default-construction!
+    solver_euler() = delete;
+
+    T step(double timestep)
+    {
+        y_cur = y_cur + f(t_cur, y_cur) * timestep;
+        t_cur += timestep;
+        return y_cur;
+    }
+};
+
+double dglfunc_exponential(double /* t */, double y)
+{
+    return params["k_exp"]*y;
+}
+
 int main()
 {
-    // initialize with default paramters
-    std::map<std::string, double> params {
-            {"stepsize", 0.001},
-            {"theta0", 0},
-            {"dotheta0", 2*M_PI}
-    };
-
     std::ifstream parmfile("params.txt");
     if (!parmfile)
     {
@@ -68,6 +101,19 @@ int main()
     for (auto p_it = params.begin(); p_it != params.end(); ++p_it)
         std::cout<<"# "<<p_it->first<<" = "<<p_it->second<<'\n';
 
+    solver_euler<double, decltype(dglfunc_exponential)*> s_euler(
+        dglfunc_exponential, params["t_init"], params["y_init"]
+    );
+
+    std::cout<<"# Writing data for in ["<<params["t_init"]<<", "<< params["t_end"]<<") with delta = "<<params["stepsize"]<<'\n';
+    std::cout<<std::scientific;
+
+    double t = params["t_init"], timestep = params["stepsize"];
+    while(t < params["t_end"])
+    {
+        std::cout<<t<<"  "<<s_euler.step(timestep)<<'\n';
+        t += timestep;
+    }
 
     return 0;
 }
