d2de3371b86df7564a8dee0a0006047b4bd73317
Alexander Korsunsky <fat.lobyte9@gmail.com>
2014-03-20 02:42:19 +0100

    Hacked double-pendulum problem into code. CHECK THIS

--- blatt2/03_doublependulum.cpp
+++ blatt2/03_doublependulum.cpp
@@ -16,19 +16,39 @@ static std::map<std::string, double> params {
         {"t_init", 0.0}, // initial conditions
         {"t_end", 50},
         {"y1_init", 0},
-        {"y2_init", 2},
+        {"y2_init", 0},
+        {"y3_init", 4},
+        {"y4_init", 2},
         {"omega", 1}
 };
 
+static double omega;
 
 std::valarray<double> dglfunc_doppelpendel(double /* t */, const std::valarray<double>& y)
 {
-    double d_theta = y[0] - y[1];
+    std::valarray<double> ret(4);
 
-    return {
-        y[1],
-        - params["omega"]*params["omega"] * std::sin(y[0])
-    };
+    double sin_dtheta = std::sin(y[0] - y[1]), cos_dtheta = std::cos(y[0] - y[1]);
+
+    double A =  (y[2]*y[3]*sin_dtheta) / (1 + sin_dtheta*sin_dtheta);
+    double B =  (y[2]*y[2] + 2*y[3]*y[3] - 2*y[2]*y[3]*cos_dtheta) / 
+                 (1 + sin_dtheta*sin_dtheta)*(1 + sin_dtheta*sin_dtheta) *
+                 sin_dtheta*cos_dtheta;
+
+    // dottheta_1 
+    ret[0] = (y[2] - y[3]*cos_dtheta) / (1 + sin_dtheta*sin_dtheta);
+    
+    // dottheta_2
+    ret[1] = (2*y[3] -y[2]*cos_dtheta) / (1 + sin_dtheta*sin_dtheta);
+    
+    // dot-p-tilde_1
+    ret[2] = -A + B - 2*omega*omega*std::sin(y[0]);
+    
+    // dot-p-tilde_2
+    ret[3] =  A - B - omega*omega*std::sin(y[1]);
+
+
+    return ret;
 }
 
 double energy(std::valarray<double> y)
@@ -46,7 +66,7 @@ int main()
     }
     else
     {
-        readParams(params, parmfile);
+        cop::readParams(params, parmfile);
         std::clog<<"Parameters loaded.\n";
     }
 
@@ -55,18 +75,23 @@ int main()
 
 	// initialize solver
     double timestep = params["stepsize"], t_end = params["t_end"];
-    std::valarray<double> y_init = {params["y1_init"], params["y2_init"]};
+    omega = params["omega"];
+    std::valarray<double> y_init = {
+        params["y1_init"], params["y2_init"], params["y3_init"], params["y4_init"]
+    };
 
 
-    solver_RungeKutta4<std::valarray<double>, decltype(dglfunc_doppelpendel)*> s_rk4(
+    cop::solver_RungeKutta4<std::valarray<double>, decltype(dglfunc_doppelpendel)*> s_rk4(
         dglfunc_doppelpendel, params["t_init"], y_init
     );
 
 	// write header line
 	std::cout<<
         "Time"<<"  "<<
-        "\\theta"<<"  "<<
-        "\\dottheta"<<"  "<<
+        "\\theta1"<<"  "<<
+        "\\theta2"<<"  "<<
+        "\\ptilde1"<<"  "<<
+        "\\ptilde2"<<"  "<<
         "energy"<<'\n';
 
     // show parameters
@@ -83,7 +108,8 @@ int main()
         if (! (stepcount % resolution))
         {
             std::cout<<s_rk4.getTime()<<"  "
-                <<s_rk4.getY()[0]<<"  "<<s_rk4.getY()[1]<<"  "<<energy(s_rk4.getY())
+                <<s_rk4.getY()[0]<<"  "<<s_rk4.getY()[1]<<"  "
+                <<s_rk4.getY()[2]<<"  "<<s_rk4.getY()[3]<<"  "
                 <<'\n';
         }
 
